b3fb5d9efd06d64f1739922e0aee54986d6f9504
MiracleDali <108646172+MiracleDali@users.noreply.github.com>
2026-02-07 17:33:06 +0800

    refactor(App): 添加了 v-bind 的各种使用方法

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,50 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div class="属性绑定">
+    <h1>属性绑定</h1>
+    <p> 
+      注：v-bind 指令将元素的 id 与组件的属性保持一致如果绑定的是 null 或者是 undefined, 
+      那么该attribute将会从渲染的元素上移除
+    </p>
+    <h3>1、属性绑定使用 v-bind:</h3>
+    <div v-bind:id="bind_id" v-bind:class="bind_class" v-bind:title="bind_title">Example：属性绑定</div>
+    <div :id="bind_id" :class="bind_class" :title="bind_title_two">Example：属性绑定简写</div> 
+    <h3>2、布尔型 可以动态控制按钮的可点击状态或者其他</h3>
+    <button :disabled="bind_button_disabled">按钮1</button>
+    <button :disabled="!bind_button_disabled">按钮2</button>
+    <h3>3、使用字典的方式绑定多个值</h3>
+    <div v-bind="objectOfAttrs">对象语法</div>
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+<script>
+export default {
+  data() {
+    return {
+      // 属性绑定
+      bind_class: "active",
+      bind_id: "bind_id",
+      bind_title: null,
+      bind_title_two: "bind_title_two",
+      // 属性绑定控制按钮
+      bind_button_disabled: false,   // false 启用按钮，true 禁用按钮
+      // 使用字典的方式绑定多个值
+      objectOfAttrs: {
+        class: 'appclass',
+        id: 'appid',
+      }  
+    }
   }
 }
+
+</script>
+
+
+<style>
+/* 属性绑定的样式 */
+#bind_id {
+  color: rgba(255, 0, 0, 0.801);
+}
 </style>
+
+
