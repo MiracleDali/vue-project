db4e36cdf4724f9c0bc19476b32e4254812c5143
MiracleDali <108646172+MiracleDali@users.noreply.github.com>
2026-02-26 23:11:04 +0800

    feat(EventDemo): 添加事件参数传递功能
    新增事件参数演示功能，包括：
    - 事件传参的基本用法
    - 事件传递参数并显示提示框
    - 事件传递参数和event对象获取点击元素信息
    - 添加names数据用于事件演示
    ```

--- src/components/EventDemo.vue
+++ src/components/EventDemo.vue
@@ -12,6 +12,16 @@
         <button @:click="addCount">调用methods快里面的方法进行处理</button>
         <p>点击计数：  {{ methods_count }}</p>
 
+
+        <h3>3、事件参数</h3>
+        <button @click="addCountEvent">事件传参</button>
+
+        <h3>4、事件传递参数</h3>
+        <button @click="transmit('这是传递的参数内容')">事件传递参数</button>
+
+        <h3>5、事件传递参数+event对象传递-点击了谁的名字</h3>
+        <p @click="getNameHandler(item, $event)" v-for="(item, index) of names" :key="index">{{ item }}</p>
+
     </div>
 </template>
 
@@ -20,7 +30,9 @@ export default {
     data() {
         return {
             count: 0,
-            methods_count: 0
+            methods_count: 0,
+
+            names: ['张三', '李四', '王五']
         }
     },
 
@@ -30,6 +42,22 @@ export default {
         addCount() {
             // 使用 this. 访问 data 中的数据
             this.methods_count++
+        },
+        
+        addCountEvent(e) {
+            console.log(e.target.innerHTML)
+            e.target.innerHTML = "事件传参" + "  刚刚点击了这个按钮"
+        },
+
+        // 事件传参
+        transmit(message) {
+            alert(message)
+        },
+
+        // 获取点击谁的名字
+        getNameHandler(name, e){
+            alert(name)
+            console.log(e)
         }
     },    
 }
