b914a2120fd56aba4cece08ad5a06394b2ecb2d3
MiracleDali <108646172+MiracleDali@users.noreply.github.com>
2026-06-19 18:27:17 +0800

    feat: 初始化Vue3项目基础配置
    添加了项目基础文件配置包括：
    - 添加.gitignore文件忽略日志、构建产物等不需要提交的文件
    - 配置VSCode推荐插件Vue.volar
    - 创建README.md包含项目说明、IDE设置建议和开发命令
    - 添加Vue2语法示例组件和基础App组件
    - 配置TypeScript客户端类型声明env.d.ts
    - 设置HTML模板index.html入口文件
    ```

---
+++ backup/02_也能写vue2的语法/components/Person.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{name}}</h2>
+        <h2>年龄：{{age}}</h2>
+        <button @click="ChangeName">修改名字</button>
+        <button @click="ChangeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+    </div>
+</template>
+
+
+<script lang="ts">
+    export default {
+        name: 'Person',
+        
+        data() {
+            return {
+                name: '张三',
+                age: 18,
+                tel: '12345678901'
+            }
+        },
+
+        methods: {
+            showTel() {
+                alert(this.tel)
+            },
+            ChangeName() {
+                this.name = 'lishi_01'
+            },
+            ChangeAge() {
+                this.age += 1
+            },
+        }
+    }
+</script>
+
+
+<style scoped>
+    .person {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button {
+        margin-left: 5px;
+    }
+</style>
