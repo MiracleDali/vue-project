fa24e52575cac99b2bd6699764b027c3cc352f1e
MiracleDali <108646172+MiracleDali@users.noreply.github.com>
2026-06-20 16:14:53 +0800

    feat: reactive_对象类型的响应数据

---
+++ backup/08_reactive_对象类型的响应数据/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- html -->
+    <Person/>
+</template>
+
+
+<script lang="ts">
+    // JS 或 TS
+    import Person from './components/Person.vue'
+
+    export default {
+        name: 'App',    // 组件名`
+        components: { Person }  // 注册组件
+    }
+</script>
+
+
+<style>
+    /* 样式 */
+</style>
