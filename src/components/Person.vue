56db1bf78777aef080d28e639989439e0e40c1cc
MiracleDali <108646172+MiracleDali@users.noreply.github.com>
2026-06-23 22:40:36 +0800

    feat计算属性-computed

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,31 +1,39 @@
 <template>
     <div class="person">
-        <h2>姓名：{{person.name}}</h2>
-        <h2>年龄：{{person.age}} {{nl}}</h2>
-        <button @click="changeName">修改姓名</button>
-        <button @click="changeAge">修改姓名</button>
+        姓：<input type="text" v-model="firstName"></input> <br>
+        名：<input type="text" v-model="lastName"></input> <br><br>
+        <button @click="changeFullName">将全名改为Li-si</button> <br><br>
+        全名：<span>{{fullName}}</span>
     </div>
 </template>
 
 
 <script setup lang="ts" name="Person123">
-    import {ref, reactive, toRefs, toRef} from 'vue'
+    // 计算属性-computed
+    import {ref, reactive, computed, toRefs, toRef} from 'vue'
+    let firstName = ref('zhang')
+    let lastName = ref('san')
 
-    // 数据
-    let person = reactive({
-        name: '张三',
-        age: 18,
+    // 计算属性-computed 且是只读的不能修改
+    // let fullName = computed(() => {
+    //     return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+    // })
+
+    // 计算属性-computed 可读可改
+    let fullName = computed({
+        // get 读
+        get() {
+            return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+        },
+        // set 写
+        set(val) {
+            const [str1 = '', str2 = ''] = val.split('-')
+            firstName.value = str1
+            lastName.value = str2
+        }
     })
-
-    let {name, age} = toRefs(person)
-    let nl = toRef(person, 'age')
-
-    // 方法
-    function changeName() {
-        name.value += '~'
-    }
-    function changeAge() {
-        age.value += 1
+    function changeFullName() {
+        fullName.value = 'Li-'
     }
 </script>
 
